a119c6ba63fa5fdb19962d7c7cc45dbd5c4277ae
Balachandar <balachandarsam2011@gmail.com>
2018-03-27 23:29:22 -0700

    skeleton code

---
+++ src/SER516_Lab2_Client/ClientMain.java
@@ -0,0 +1,19 @@
+package SER516_Lab2_Client;
+
+
+class ClientMain {
+
+
+
+	public static void initialiseGUI()
+	{
+
+	}
+
+	public static void main(String[] args){
+
+		initialiseGUI();
+	}
+
+
+}
---
+++ src/SER516_Lab2_Server/ServerMain.java
@@ -0,0 +1,22 @@
+package SER516_Lab2_Server;
+
+
+import java.awt.*;
+import java.io.IOException;
+
+class ServerMain {
+
+ /*
+  * This class acts the main controller to start the server GUI
+  */
+	public static void initialiseGUI()
+	{
+
+	}
+
+	public static void main(String[] args) throws InterruptedException, IOException {
+
+		initialiseGUI();
+
+	}
+}
