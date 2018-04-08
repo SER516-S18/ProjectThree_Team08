ff2edad4b6f4356c62c77fe2cb6f3ad646aed418
JasonRice <jarice6@asu.edu>
2018-04-08 15:23:57 -0700

    changed the ServerConnectionPanelLogic to implement an observer.

--- src/main/client/controller/ServerConnectionPanelLogic.java
+++ src/main/client/controller/ServerConnectionPanelLogic.java
@@ -9,16 +9,37 @@ import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 
+import main.model.EmotionMessageBean;
+
 /**
  * Server conection panel logic
  * @author Jason Rice
  * @version 1.0
  */
-public class ServerConnectionPanelLogic{
+public class ServerConnectionPanelLogic implements Observer{
 	JTextField ipAddressTextField;
 	JTextField portTextField;
 	JButton startStopButton;
 	JLabel timeStampValueLabel;
+	EmotionMessageBean emotionMessageBean;
+	
+	/**
+	 * Constructor sets the fields that need to be updated and sets the reference to the observable.
+	 * @param emotionMessageBean
+	 * @param ipAddressTextField
+	 * @param portTextField
+	 * @param startStopButton
+	 * @param timeStampValueLabel
+	 */
+	public ServerConnectionPanelLogic(EmotionMessageBean emotionMessageBean,
+			JTextField ipAddressTextField, JTextField portTextField, JButton startStopButton, JLabel timeStampValueLabel){
+		this.emotionMessageBean = emotionMessageBean;
+		this.ipAddressTextField = ipAddressTextField;
+		this.portTextField = portTextField;
+		this.startStopButton = startStopButton;
+		this.timeStampValueLabel = timeStampValueLabel;
+		this.emotionMessageBean.addObserver(this);
+	}
 	
 	/**
 	 * Constructor sets the fields that need to be updated.
@@ -51,5 +72,15 @@ public class ServerConnectionPanelLogic{
 				System.out.println("port: " + portTextField.getText());
 			}
 		};
+	}
+
+	/**
+	 * update the fields in the SeverConnectionPanel.
+	 */
+	@Override
+	public void update(Observable observable, Object arg) {
+		if(this.emotionMessageBean == observable){
+			this.timeStampValueLabel.setText(""+this.emotionMessageBean.getClockTick());
+		}
 	}
 }
