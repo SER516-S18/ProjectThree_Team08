e89bcebe9716b83f203b40733f4f7fa73e2ec49d
Balachandar <balachandarsam2011@gmail.com>
2018-04-10 22:33:52 -0700

    Added JavaDocs

--- src/main/server/controller/Server.java
+++ src/main/server/controller/Server.java
@@ -6,40 +6,27 @@ import main.utils.ConnectionConstants;
 import javax.websocket.DeploymentException;
 
 import java.util.Scanner;
-
+/**
+ * Server Class which starts the server application
+ * @author Balachandar Sampath
+ * @version 1.0
+ */
 public class Server {
 
+    /**
+     * Main Function which initialises the Server
+     * @param args
+     */
     public static void main(String[] args) {
-
-        // Initialise the server
-        EndpointController endpointController = EndpointController.getInstance();
-        UIController uiController = UIController.getInstance();
 
         // Initilaise the gui
         ServerInitUI.initializeGUI();
-
-
         org.glassfish.tyrus.server.Server server = new
                 org.glassfish.tyrus.server.Server(ConnectionConstants.HOSTNAME,
                 ConnectionConstants.PORT, "/"+ConnectionConstants.ROOT_PATH, ServerEndpoint.class);
         try {
             server.start();
-            int input = Integer.parseInt(new Scanner(System.in).nextLine());
-            while(input!=4) {
-                switch (input)
-                {
-                    case 1:
-                        EndpointController.getInstance().sendOnce();
-                        break;
-                    case 2:
-                        EndpointController.getInstance().sendInIntervals(1.5);
-                        break;
-                    case 3:
-                        EndpointController.getInstance().stop();
-                        break;
-                }
-                input = Integer.parseInt(new Scanner(System.in).nextLine());
-            }
+            new Scanner(System.in).nextLine();
         } catch (DeploymentException e) {
             throw new RuntimeException(e);
         } finally {
